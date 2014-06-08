c2c766216f6822d4c7b79e0d4457193f9d579e36
jtravisnorton <jtravisnorton@gmail.com>
2014-06-08 17:26:24 -0400

    Update get_points.js

--- static/js/get_points.js
+++ static/js/get_points.js
@@ -7,7 +7,16 @@ function getPoints (group){
         async: false,
         success: function(data){points=data}
     });
-    group.point_group = L.geoJson(points);
+    group.point_group = L.geoJson(points, {
+        onEachFeature: function (feature, layer) {
+            layer.on('mouseover', function(e){
+                e.target.setZIndexOffset(250);
+            });
+            layer.on('mouseout', function(e){
+                e.target.setZIndexOffset(0);
+            });
+        }
+    });
 }
 
 function Group(geojson_path, name){
@@ -24,8 +33,11 @@ function Group(geojson_path, name){
     var counties = [];
     var pointMap = [];
   	this.point_group.eachLayer(function(layer){
+        var icon = L.MakiMarkers.icon({icon: "pharmacy", color: "#80b3d3", size: "l"});
+        layer.setIcon(icon);
         var feature = layer.toGeoJSON();
-        name = feature.id + "/" + feature.properties.street + "/" + feature.properties.citystatezip;
+        layer.bindPopup("<td><strong>"+feature.id+"</strong><br>"+feature.properties.street+" "+feature.properties.citystatezip+"<br><a href='#address-input'>get directions</a></td>");
+        name = feature.id + "." + feature.properties.street + "." + feature.properties.citystatezip;
         pointMap[name] = {layer: layer, latlng: layer.getLatLng()};
         if (cities[feature.properties.city] == null){
             cities[feature.properties.city] = {count: 1, x: feature.geometry.coordinates[0], y: feature.geometry.coordinates[1]};
@@ -53,6 +65,7 @@ function Group(geojson_path, name){
     var county_group = L.featureGroup();
     for (var county in this.counties){
       var latlng = L.latLng(this.counties[county].y, this.counties[county].x);
+      var icon = L.MakiMarkers.icon({icon: "building", color: "#b0b", size: "m"});
       var county_marker = L.marker(latlng,{ 
           icon: L.divIcon({
           // specify a class name that we can refer to in styles, as we
@@ -72,29 +85,22 @@ function Group(geojson_path, name){
     var city_group = L.featureGroup();
     for (var city in this.cities){
       var latlng = L.latLng(this.cities[city].y, this.cities[city].x);
+      var icon = L.MakiMarkers.icon({icon: "town-hall", color: "#5e4fa2", size: "l"});
+      icon.html = "hello";
       var city_marker = L.marker(latlng,{
-          icon: L.divIcon({
-          // specify a class name that we can refer to in styles, as we
-          // do above.
-          className: 'count-icon',
-          // html here defines what goes in the div created for each marker
-          html: this.cities[city].count,
-          // and the marker width and height
-          iconSize: [40, 40]
-      })});
+          icon: icon, riseOnHover: true});
       city_marker.bindPopup(city);
       city_group.addLayer(city_marker);
+      city_marker.on('click', function(e){
+          map.panTo(e.latlng);
+          map.setZoom(zoom_points);
+      });
+      city_marker.on('mouseover', function(e){
+          e.target.openPopup();
+      });
+      city_marker.on('mouseout', function(e){
+          e.target.closePopup();
+      });
     }
     this.city_group = city_group;
-    console.log(this.pointMap)
-}
-
-//returns points visible according to current map bounds
-function popInvisiblePoints(group, map){
-  var bounds = map.getBounds();
-  for (point in group.pointMap){
-    if (bounds.contains(group.pointMap[point].latlng == false)){
-      map.removeLayer(group.pointMap[point].layer);
-    }
-  };
 }
